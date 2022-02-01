df77ee1fe6f4a504b0bb5e7208074dde8265d0c2
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 21:44:26 +0530

    [Rakshit] Add: Reverse Number program completed

--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -13,7 +13,7 @@ namespace LogicalProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter 1: fibonacci series\n" + "Enter 2: Perfect Number\n" + "Enter 3: Prime Number\n" + "Enter 4: Exit\n");
+                Console.WriteLine("Enter 1: fibonacci series\n" + "Enter 2: Perfect Number\n" + "Enter 3: Prime Number\n" + "Enter 4: Reverse Number\n" + "Enter 5: Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -30,6 +30,10 @@ namespace LogicalProblems
                         primeNumber.Prime();
                         break;
                     case 4:
+                        ReverseNumber reverseNumber = new ReverseNumber();
+                        reverseNumber.Reverse();
+                        break;
+                    case 5:
                         flag = false;
                         break;
 
---
+++ LogicalProblems/ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.WriteLine("Enter the Number\n");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int number = 0;
+            while (num > 0)
+            {
+                int i = num % 10;
+                number = number * 10 + i;
+                num /= 10;
+            }
+            Console.WriteLine("The Reverse Of a Given Number is " + number);
+
+        }
+    }
+}
