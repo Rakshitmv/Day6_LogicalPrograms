26c41611ca477181b313f2ccb7e97048fcbb7c28
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 22:02:26 +0530

    [Rakshit] Add: Stop Watch program completed

--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -13,7 +13,7 @@ namespace LogicalProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter 1: fibonacci series\n" + "Enter 2: Perfect Number\n" + "Enter 3: Prime Number\n" + "Enter 4: Reverse Number\n" + "Enter 5: To generate Coupons\n" + "Enter 6: Exit\n");
+                Console.WriteLine("Enter 1: fibonacci series\n" + "Enter 2: Perfect Number\n" + "Enter 3: Prime Number\n" + "Enter 4: Reverse Number\n" + "Enter 5: To generate Coupons\n" + "Enter 6: Stop Watch\n" + "Enter 7: Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -38,6 +38,10 @@ namespace LogicalProblems
                         couponNumbers.Coupon();
                         break;
                     case 6:
+                        StopWatch stopWatch = new StopWatch();  
+                        stopWatch.Watch();
+                        break;
+                    case 7:
                         flag = false;
                         break;
 
