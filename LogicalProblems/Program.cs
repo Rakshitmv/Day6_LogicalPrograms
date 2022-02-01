0a6d3d6ef6e5fce576269a62ec358baadfb0d2d5
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 21:22:32 +0530

    [Rakshit] Add: Fibonacci series program completed

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter 1: fibonacci series\n" + "Enter 2: Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                   case 1:
+                        Fibonacci fibonacci = new Fibonacci();
+                        fibonacci.CheckFibonacci();
+                        break;
+                  
+                    case 2:
+                        flag = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please enter the correct option\n");
+                        break;
+                }
+
+
+
+            }
+            
+
+        }
+    }
+}
